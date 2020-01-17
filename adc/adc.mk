e0f062e283524afedfff4c46b542bf9e569c2024
John Diamond <jdiamond@adlinux.fnal.gov>
2020-01-17 12:00:15 -0600

    Added some binaries to .gitignore.

--- adc/adc.mk
+++ adc/adc.mk
@@ -1,6 +1,6 @@
 ALL_CLEAN   += adc_clean
 ALL_TARGETS +=
-ALL_TEST    += adc_test
+ALL_TEST    += adc_tests
 
 adc: adc/ADCDriverStub.o
 
@@ -9,6 +9,6 @@ adc_clean:
 	-rm -f output/host/adc/*.o
 	-rm -f output/target/adc/*.o
 
-adc_test: adc/ADCDriverStub.o adc/ADCDriverStubTest.o adc/AllTests.o adc/IADCDriver.o
-	$(HOST_CXX) output/host/adc/IADCDriver.o output/host/adc/ADCDriverStub.o output/host/adc/ADCDriverStubTest.o output/host/adc/AllTests.o -o output/host/adc/tests $(EES_LDFLAGS) $(DEV_OBJS) -L$(MYLIBS) $(DEV_LIBS) $(TEST_FLAGS)
-	./output/host/adc/tests
+adc_tests: adc/ADCDriverStub.o adc/ADCDriverStubTest.o adc/AllTests.o adc/IADCDriver.o
+	$(HOST_CXX) output/host/adc/IADCDriver.o output/host/adc/ADCDriverStub.o output/host/adc/ADCDriverStubTest.o output/host/adc/AllTests.o -o output/host/adc/adc_tests $(EES_LDFLAGS) $(DEV_OBJS) -L$(MYLIBS) $(DEV_LIBS) $(TEST_FLAGS)
+	./output/host/adc/adc_tests
