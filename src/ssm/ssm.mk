6dc0cdc826f07c4b6830baf75fdfc2b975296097
John Diamond <jdiamond@adlinux.fnal.gov>
2020-01-30 21:42:21 -0600

    Refactored the way we handle object file lists to be cleaner.

--- src/ssm/ssm.mk
+++ src/ssm/ssm.mk
@@ -8,6 +8,15 @@ ALL_OUT         += $(SSM_HOST_OUT) $(SSM_TARGET_OUT)
 ALL_SOURCES     += $(wildcard ssm/*.C)
 ALL_HEADERS     += $(wildcard ssm/*.H)
 
+SSM_OBJS        = ISSMDeviceDriver.o SSMDeviceDriverMock.o
+SSM_OBJS_PREFIX = $(addprefix ssm/,$(SSM_OBJS))
+SSM_OBJS_HOST   = $(addprefix $(SSM_HOST_OUT)/,$(SSM_OBJS))
+SSM_OBJS_TARGET = $(addprefix $(SSM_TARGET_OUT)/,$(SSM_OBJS))
+
+SSM_TEST_OBJS   = AllTests.o SSMDeviceDriverMockTest.o 
+SSM_TEST_OBJS_PREFIX = $(addprefix ssm/,$(SSM_TEST_OBJS))
+SSM_TEST_OBJS_HOST = $(addprefix $(SSM_HOST_OUT)/,$(SSM_TEST_OBJS))
+
 $(SSM_TARGET_OUT):
 	$(EES_OUT) mkdir -p $(SSM_TARGET_OUT)
 
@@ -18,27 +27,22 @@ ssm_clean:
 	$(EES_OUT) rm -f ssm/*.o
 
 # Host ssm library
-$(SSM_HOST_OUT)/ssm.a: ssm/ISSMDeviceDriver.o ssm/SSMDeviceDriverMock.o
+$(SSM_HOST_OUT)/ssm.a: $(SSM_OBJS_PREFIX)
 	@echo "-m-> Linking $@ (host)..."
-	$(EES_OUT) $(HOST_AR) ru $@ \
-		$(SSM_HOST_OUT)/ISSMDeviceDriver.o \
-		$(SSM_HOST_OUT)/SSMDeviceDriverMock.o
+	$(EES_OUT) $(HOST_AR) ru $@ $(SSM_OBJS_HOST)
 	$(EES_OUT) $(HOST_RANLIB) $@
 
 # Target ssm library
-$(SSM_TARGET_OUT)/ssm.a: ssm/ISSMDeviceDriver.o ssm/SSMDeviceDriverMock.o
-	@echo "-m-> Linking $@ (host)..."
-	$(EES_OUT) $(AR) ru $@ \
-		$(SSM_TARGET_OUT)/ISSMDeviceDriver.o \
-		$(SSM_TARGET_OUT)/SSMDeviceDriverMock.o
+$(SSM_TARGET_OUT)/ssm.a: $(SSM_OBJS_PREFIX)
+	@echo "-m-> Linking $@ (target)..."
+	$(EES_OUT) $(AR) ru $@ $(SSM_OBJS_TARGET)
 	$(EES_OUT) $(RANLIB) $@
 
 # Unit test suite
-ssm_tests: $(SSM_HOST_OUT)/ssm.a ssm/AllTests.o ssm/SSMDeviceDriverMockTest.o 
+ssm_tests: $(SSM_HOST_OUT)/ssm.a $(SSM_TEST_OBJS_PREFIX)
 	@echo "-m-> Linking $@ (host)..."
 	$(EES_OUT) $(HOST_CXX) -o $(SSM_HOST_OUT)/ssm_tests \
-		$(SSM_HOST_OUT)/AllTests.o \
-		$(SSM_HOST_OUT)/SSMDeviceDriverMockTest.o \
+		$(SSM_TEST_OBJS_HOST) \
 		$(SSM_HOST_OUT)/ssm.a \
 		$(DEV_LIBS) $(TEST_FLAGS)
 	@echo "-m-> Running $@..."
