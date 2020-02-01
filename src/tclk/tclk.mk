2b50c8d3881de341747762aac886654c837794f3
John Diamond <jdiamond@adlinux.fnal.gov>
2020-02-01 07:20:45 -0600

    Refactored the tclk module object lists to be cleaner.

--- src/tclk/tclk.mk
+++ src/tclk/tclk.mk
@@ -8,6 +8,15 @@ ALL_OUT         += $(TCLK_HOST_OUT) $(TCLK_TARGET_OUT)
 ALL_SOURCES     += $(wildcard tclk/*.C)
 ALL_HEADERS     += $(wildcard tclk/*.H)
 
+TCLK_OBJS        = ITCLKDecoderDriver.o TCLKDecoderDriverMock.o 
+TCLK_OBJS_PREFIX = $(addprefix tclk/,$(TCLK_OBJS))
+TCLK_OBJS_HOST   = $(addprefix $(TCLK_HOST_OUT)/,$(TCLK_OBJS))
+TCLK_OBJS_TARGET = $(addprefix $(TCLK_TARGET_OUT)/,$(TCLK_OBJS))
+
+TCLK_TEST_OBJS   = AllTests.o TCLKDecoderDriverMockTest.o
+TCLK_TEST_OBJS_PREFIX = $(addprefix tclk/,$(TCLK_TEST_OBJS))
+TCLK_TEST_OBJS_HOST = $(addprefix $(TCLK_HOST_OUT)/,$(TCLK_TEST_OBJS))
+
 $(TCLK_TARGET_OUT):
 	$(EES_OUT) mkdir -p $(TCLK_TARGET_OUT)
 
@@ -17,14 +26,13 @@ $(TCLK_HOST_OUT):
 tclk_clean:
 	$(EES_OUT) rm -f tclk/*.o
 
-tclk_tests: $(BIN_DIR) tclk/ITCLKDecoderDriver.o tclk/TCLKDecoderDriverMock.o tclk/AllTests.o tclk/TCLKDecoderDriverMockTest.o
+tclk_tests: $(TCLK_OBJS_PREFIX) $(TCLK_TEST_OBJS_PREFIX)
 	@echo "-m-> Linking $@ (host)..."
 	$(EES_OUT) $(HOST_CXX) -o $(TCLK_HOST_OUT)/tclk_tests \
-		$(TCLK_HOST_OUT)/ITCLKDecoderDriver.o \
-		$(TCLK_HOST_OUT)/TCLKDecoderDriverMock.o \
-		$(TCLK_HOST_OUT)/AllTests.o \
-		$(TCLK_HOST_OUT)/TCLKDecoderDriverMockTest.o \
+		$(TCLK_OBJS_HOST) \
+		$(TCLK_TEST_OBJS_HOST) \
 		$(DEV_LIBS) $(TEST_FLAGS)
 	@echo "-m-> Running $@..."
 	@./$(TCLK_HOST_OUT)/tclk_tests
-
+	@echo "-m-> Coverage report for $(TCLK_OBJS)..."
+	$(EES_OUT) $(HOST_GCOV) $(TCLK_OBJS_HOST)
