50e1519a1c66b1727c7993b8c4a83eb05e00479a
John Diamond <jdiamond@adlinux.fnal.gov>
2020-02-02 08:01:04 -0600

    Refactored mu2eerd makefile.  Added main.o to coverage report.

--- src/mu2eerd/mu2eerd.mk
+++ src/mu2eerd/mu2eerd.mk
@@ -1,12 +1,22 @@
 MU2EERD_HOST_OUT   = $(HOST_BIN_DIR)/mu2eerd
 MU2EERD_TARGET_OUT = $(TARGET_BIN_DIR)/mu2eerd
 
+MU2EERD_LIBS        = ssm.a
+MU2EERD_LIBS_HOST   = $(addprefix $(HOST_BIN_DIR)/,$(MU2EERD_LIBS))
+MU2EERD_LIBS_TARGET = $(addprefix $(TARGET_BIN_DIR)/,$(MU2EERD_LIBS))
+
+MU2EERD_OBJS        = main.o
+MU2EERD_OBJS_PREFIX = $(addprefix mu2eerd/,$(MU2EERD_OBJS))
+MU2EERD_OBJS_HOST   = $(addprefix $(MU2EERD_HOST_OUT)/,$(MU2EERD_OBJS))
+MU2EERD_OBJS_TARGET = $(addprefix $(MU2EERD_TARGET_OUT)/,$(MU2EERD_OBJS))
+
 ALL_CLEAN       += mu2eerd_clean
 ALL_TARGETS     += mu2eerd
 ALL_TEST        += 
 ALL_OUT         += $(MU2EERD_HOST_OUT) $(MU2EERD_TARGET_OUT)
 ALL_SOURCES     += $(wildcard mu2eerd/*.C)
 ALL_HEADERS     += $(wildcard mu2eerd/*.H)
+ALL_COVERAGE    += $(MU2EERD_OBJS_HOST)
 
 $(MU2EERD_TARGET_OUT):
 	$(EES_OUT) mkdir -p $(MU2EERD_TARGET_OUT)
@@ -17,12 +27,14 @@ $(MU2EERD_HOST_OUT):
 mu2eerd_clean:
 	$(EES_OUT) rm -f mu2eerd/*.o
 
-mu2eerd: mu2eerd/main.o
+mu2eerd: $(MU2EERD_OBJS_PREFIX) $(MU2EERD_LIBS_HOST) $(MU2EERD_LIBS_TARGET)
 	@echo "-m-> Linking $@ (target)..."
 	$(EES_OUT) $(CXX) -o $(MU2EERD_TARGET_OUT)/mu2eerd \
-		$(MU2EERD_TARGET_OUT)/main.o \
+		$(MU2EERD_OBJS_TARGET) \
+		$(MU2EERD_LIBS_TARGET) \
 		$(EES_LDFLAGS) -L$(MYLIBS) $(DEV_LIBS)
 	@echo "-m-> Linking $@ (host)..."
 	$(EES_OUT) $(HOST_CXX) -o $(MU2EERD_HOST_OUT)/mu2eerd \
-		$(MU2EERD_HOST_OUT)/main.o \
-		$(EES_LDFLAGS) -L$(MYLIBS) $(DEV_LIBS) $(TEST_FLAGS)
+		$(MU2EERD_OBJS_HOST) \
+		$(MU2EERD_LIBS_HOST) \
+		$(DEV_LIBS) $(TEST_FLAGS)
