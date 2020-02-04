eaaaae462e33caeee3ee490ee6f523143f6ff584
John Diamond <jdiamond@adlinux.fnal.gov>
2020-02-04 16:01:17 -0600

    Deployment script for test node.

---
+++ src/deploy-test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Jenkins build area
+STABLE_BUILD=/usr/local/products/buildroot/achilles_mu2eer
+
+# Test node
+TEST_NODE=srsd
+
+# Local of boot images on nova
+IMAGE_LOCATION=/fecode-bd/linux_boot/ees/srs
+
+echo "Deploying mu2eerd_buildroot build #?? to $TEST_NODE...";
+
+scp $STABLE_BUILD/output/images/zImage nova:$IMAGE_LOCATION/zImage
+scp $STABLE_BUILD/output/images/linuxDT.dtb nova:$IMAGE_LOCATION/linuxDT.dtb
+
+echo "Done!";
