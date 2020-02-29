4ddb92b5a4267e7bb636ac1a86a1408959aa49ad
John Diamond <jdiamond@adlinux.fnal.gov>
2020-02-29 10:27:13 -0600

    Refactored scp to be quiet.

--- src/deploy-test.sh
+++ src/deploy-test.sh
@@ -12,7 +12,16 @@ IMAGE_LOCATION=/fecode-bd/linux_boot/ees/srs
 # Copy kernel+rootfs image and device tree to nova TFTP area...
 echo "Deploying mu2eerd_buildroot build #?? to $TEST_NODE...";
 
-scp $STABLE_BUILD/output/images/zImage nova:$IMAGE_LOCATION/zImage
-scp $STABLE_BUILD/output/images/linuxDT.dtb nova:$IMAGE_LOCATION/linuxDT.dtb
+ZIMAGE_SOURCE=$STABLE_BUILD/output/images/zImage
+ZIMAGE_DEST=nova:$IMAGE_LOCATION/zImage
+printf "Copy $ZIMAGE_SOURCE to $ZIMAGE_DEST...";
+scp $STABLE_BUILD/output/images/zImage nova:$IMAGE_LOCATION/zImage 2>/tmp/scp_stderr 1>/tmp/scp_stdout
+printf " success!\n";
+
+DT_SOURCE=$STABLE_BUILD/output/images/linuxDT.dtb
+DT_DEST=nova:$IMAGE_LOCATION/linuxDT.dtb
+printf "Copy $DT_SOURCE to $DT_DEST...";
+scp $STABLE_BUILD/output/images/linuxDT.dtb nova:$IMAGE_LOCATION/linuxDT.dtb 2>/tmp/scp_stderr 1>/tmp/scp_stdout
+printf " success!\n";
 
 echo "Done!";
