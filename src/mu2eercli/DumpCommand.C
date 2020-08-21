53a2aa4d24a200a018b4911a8f72f693b49114cf
radkins <radkins@fnal.gov>
2020-08-21 09:42:16 -0400

    Implement run function

--- src/mu2eercli/DumpCommand.C
+++ src/mu2eercli/DumpCommand.C
@@ -7,18 +7,29 @@
  */
 
  #include <iostream>
+ #include <fstream>
 
  #include "DumpCommand.H"
 
  using namespace Mu2eER;
  using namespace std;
 
- DumpCommand::DumpCommand( ControlMQClient& mqc, SharedMemoryClient& shmc )
-   : Command( mqc, shmc )
- {
- }
+DumpCommand::DumpCommand( ControlMQClient& mqc, SharedMemoryClient& shmc )
+  : Command( mqc, shmc )
+{
+}
 
- void DumpCommand::run( unsigned int argc, const char* argv[] )
- {
-   cout << _shmc.pidGet() << endl;
- }
+void DumpCommand::run( unsigned int argc, const char* argv[] )
+{
+  int i = 0;
+  int size = _shmc.ssmBlockGet().dataSizeGet();
+  const int* arr = _shmc.ssmBlockGet().dataGet();
+
+  std::ofstream fileToCreate("shared_memory.csv");
+  fileToCreate << "Data" << endl;
+  for (i = 0; i < size; i++) {
+    fileToCreate << arr[i] << endl;
+  }
+
+  fileToCreate.close();
+}
