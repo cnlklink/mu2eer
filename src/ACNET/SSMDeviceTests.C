7c6a8c2620db71ca3035ce5cd238a63937481185
radkins <radkins@fnal.gov>
2020-09-04 10:06:25 -0400

    Implement IdealSpillRead test

--- src/ACNET/SSMDeviceTests.C
+++ src/ACNET/SSMDeviceTests.C
@@ -96,14 +96,14 @@ TEST( CoreGroup, SpillCountReadInitial )
   CHECK_THROWS( AcnetError, deviceB.spillCounterRead( dest, &request ) );
 
   // Handle bad offset
-  Array<SSMDevice::spill_counter_read_t> destB( &buf, 
-                                                Index( SSMDevice::SPILL_COUNTER_READING_MAX + 1 ), 
+  Array<SSMDevice::spill_counter_read_t> destB( &buf,
+                                                Index( SSMDevice::SPILL_COUNTER_READING_MAX + 1 ),
                                                 Count( 1 ) );
   CHECK_THROWS( AcnetError, device.spillCounterRead( destB, &request ) );
 
   // Handle bad length
-  Array<SSMDevice::spill_counter_read_t> destC( &buf, 
-                                                Index( 0 ), 
+  Array<SSMDevice::spill_counter_read_t> destC( &buf,
+                                                Index( 0 ),
                                                 Count( SSMDevice::SPILL_COUNTER_READING_MAX + 1 ) );
   CHECK_THROWS( AcnetError, device.spillCounterRead( destC, &request ) );
 }
@@ -119,19 +119,19 @@ TEST( CoreGroup, Control )
   ReqInfo request;
   const SSMDevice::control_t buf = { SSMDevice::CONTROL_START };
   SSMDevice device( "/mu2eer_test", "mu2eer_test" );
-  
+
   // Verify the devide handles a bad offset
   Array<const SSMDevice::control_t> srcA( &buf, Index( 1 ), Count( 1 ) );
   CHECK_THROWS( AcnetError, device.statusCtrlWrite( srcA, &request ) );
-  
+
   // Verify the device handles a bad length
   Array<const SSMDevice::control_t> srcB( &buf, Index( 0 ), Count( 2 ) );
   CHECK_THROWS( AcnetError, device.statusCtrlWrite( srcB, &request ) );
-      
+
   // Verify the device handles a bad command
   const SSMDevice::control_t bufBad = { static_cast<SSMDevice::control_t>( 65535 ) };
   Array<const SSMDevice::control_t> srcC( &bufBad, Index( 0 ), Count( 1 ) );
-  CHECK_THROWS( AcnetError, device.statusCtrlWrite( srcC, &request ) );  
+  CHECK_THROWS( AcnetError, device.statusCtrlWrite( srcC, &request ) );
 }
 
 /**
@@ -145,15 +145,15 @@ TEST( CoreGroup, ControlStart )
   ReqInfo request;
   const SSMDevice::control_t buf = { SSMDevice::CONTROL_START };
   SSMDevice device( "/mu2eer_test", "mu2eer_test" );
-  
+
   // Verify that we are in the IDLE state
   SharedMemoryClient smc( Controller::TEST_DAEMON_SHM_NAME );
   CHECK_EQUAL( SSM_IDLE, smc.ssmBlockGet().currentStateGet() );
-  
+
   // Send
   Array<const SSMDevice::control_t> src( &buf, Index( 0 ), Count( 1 ) );
   device.statusCtrlWrite( src, &request );
-  
+
   // After starting the SSM mock driver should run this it's spill cycles and end
   // in the FAULT state
   smc.waitForSSMState( SSM_FAULT, 100, 10 );
@@ -171,15 +171,15 @@ TEST( CoreGroup, ControlReset )
   ReqInfo request;
   const SSMDevice::control_t buf = { SSMDevice::CONTROL_RESET };
   SSMDevice device( "/mu2eer_test", "mu2eer_test" );
-  
+
   // Verify that we are in the IDLE state
   SharedMemoryClient smc( Controller::TEST_DAEMON_SHM_NAME );
   CHECK_EQUAL( SSM_IDLE, smc.ssmBlockGet().currentStateGet() );
-  
+
   // First, start the SSM
   ControlMQClient cmq( Controller::TEST_DAEMON_CMQ_NAME );
   cmq.start();
-  
+
   // After starting the SSM mock driver should run this it's spill cycles and end
   // in the FAULT state
   smc.waitForSSMState( SSM_FAULT, 100, 10 );
@@ -192,4 +192,40 @@ TEST( CoreGroup, ControlReset )
   // And the SSM mock driver should return to the IDLE state
   smc.waitForSSMState( SSM_IDLE, 100, 10 );
   CHECK_EQUAL( SSM_IDLE, smc.ssmBlockGet().currentStateGet() );
+}
+
+/**
+ * Ideal Spill Read Test
+ *
+ * Test the Ideal Spill device reading property
+ */
+TEST( CoreGroup, IdealSpillReadInitial )
+{
+  // Construct an ACNET request and response buffer
+  ReqInfo request;
+  SSMDevice::ideal_spill_read_t buf;
+  Array<SSMDevice::ideal_spill_read_t> dest( &buf, Index( 0 ), Count( 1 ) );
+
+  // Read spill counter, should return 0 in buf
+  SSMDevice device( "/mu2eer_test", "mu2eer_test" );
+  device.idealSpillRead( dest, &request );
+  // just check that it's 16000 to 0?
+  CHECK_EQUAL( 0, buf );
+
+
+  // Handle no shared memory by throwing Ex_DEVFAILED
+  SSMDevice deviceB( "/mu2eer_test", "does_not_exist" );
+  CHECK_THROWS( AcnetError, deviceB.spillCounterRead( dest, &request ) );
+
+  // Handle bad offset
+  Array<SSMDevice::spill_counter_read_t> destB( &buf,
+                                                Index( SSMDevice::IDEAL_SPILL_READING_MAX + 1 ),
+                                                Count( 1 ) );
+  CHECK_THROWS( AcnetError, device.spillCounterRead( destB, &request ) );
+
+  // Handle bad length
+  Array<SSMDevice::spill_counter_read_t> destC( &buf,
+                                                Index( 0 ),
+                                                Count( SSMDevice::IDEAL_SPILL_READING_MAX + 1 ) );
+  CHECK_THROWS( AcnetError, device.spillCounterRead( destC, &request ) );
 }
